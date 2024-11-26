389c8faed00e26a7b99b81aa2aa4e8befb2084c1
dhvl15 <dhaval.jariwala98@gmail.com>
2024-11-26 04:01:16 -0800

    Dashboard v1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import spacy_streamlit
+import os
+import spacy
+from spacy_streamlit import load_model
 
 st.title("IE 7945 - Capstone Project")
 tab1, tab2, tab3, tab4 = st.tabs(["HOME", "EDA", "NER", "RESULTS"])
@@ -20,13 +24,45 @@ with tab2:
         """,
         height=800,
     )
-    #st.components.html("https://app.powerbi.com/links/SDbL2VLHaD?ctid=a8eec281-aaa3-4dae-ac9b-9a398b9215e7&pbi_source=linkShare/embed", width = 500, height = 200, scrolling = True)
     
 
 with tab3:
     st.header("Named Entity Recognition")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+
+    file = open('text2.txt')
+    text = file.read()
+    file.close()
+    nlp = spacy.load("custom_ner_model")
+    doc = nlp(text)
+
+    colors = {
+        "VENDOR": "#ffca80",
+        "PRODUCT": "#ff80bf",
+        "FEATURE": "#ff9580",
+        "ACTOR": "#80ffea",
+        "EXPLOIT" : "#ffff80",
+        "MODULE" : "#8aff80",
+        "VULNERABILITY NAME" : "#9580ff"  
+    }
+
+    spacy_streamlit.visualize_ner(
+        doc,
+        labels=['VENDOR', 'PRODUCT', 'FEATURE', 'ACTOR', 'EXPLOIT', 'MODULE', 'VULNERABILITY NAME'],
+        show_table=False,
+        title="Manual visualization of Tags",
+        colors=colors,
+        #manual=True
+    )
 
 with tab4:
     st.header("Results")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+   
+    url = "pages/knowledge_graph22.html"
+
+    with open(url, 'r') as file:
+        html_content = file.read()
+        st.components.v1.html(html_content, height=800)
+
+    # st.components.v1.html(url.read,
+    #     height=800,
+    # )
