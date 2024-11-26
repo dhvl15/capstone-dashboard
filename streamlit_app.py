5934ec1b9dbbad0f880a4811be28b9131611701a
dhvl15 <dhaval.jariwala98@gmail.com>
2024-11-26 00:11:56 -0800

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,17 +3,14 @@ import streamlit as st
 tab1, tab2, tab3, tab4 = st.tabs(["HOME", "EDA", "NER", "RESULTS"])
 
 with tab1:
-    st.header("IE7945 - Capstone roject")
+    st.header("IE7945 - Capstone Project")
     st.image("https://media.zenfs.com/en/newsfile_64/bc71d8ce103e8469e0c829ce0c349ccd", width=200)
     st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_whP2XVgmkIVOyrIuAb5D4PtUaMOpq4R-Yw&s", width=200)
 
-
-
 with tab2:
     st.header("Exploratory Data Analysis")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+    st.components.v1.iframe("https://github.com", width = 500, height = 200, scrolling = True)
 
-    
 with tab3:
     st.header("Named Entity Recognition")
     st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
