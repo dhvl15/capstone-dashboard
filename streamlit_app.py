a1dab80ad7542c4fccd0fb2242c854dd9cb2becf
Dhaval Jariwala <dhaval.jariwala98@gmail.com>
2024-11-25 23:50:16 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+st.set_page_config(page_title='mehul gupta\'s portfolio' ,layout="wide",page_icon='👨‍🔬')
+
+st.sidebar.markdown(info['Stackoverflow_flair'],unsafe_allow_html=True)
+
+st.header('My Debut book on Generative AI is out !!')
+st.info("""LangChain in your Pocket: Beginner's Guide to Building Generative AI Applications using LLMs""")
+
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
